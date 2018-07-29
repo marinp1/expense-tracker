f32fe468fd79c1fe1e83744465f103d6caf10eb2
Patrik Marin <patrik.marin@gmail.com>
2018-07-29 17:43:32 +0300

    Refactor transaction view

--- src/app/TransactionView/index.tsx
+++ src/app/TransactionView/index.tsx
@@ -54,42 +54,35 @@ const DayTitle = styled.p`
   font-weight: light;
 `;
 
+enum GROUPING_STYLE {
+  BY_DATE = 'BY_DATE',
+  BY_MONTH = 'BY_MONTH',
+}
+
 /* tslint:disable no-console forin */
+function groupTransactions(transactions: Transaction[], groupingStyle: GROUPING_STYLE) {
+  
+  const dateMapped = transactions.map(t => ({
+    ...t,
+    formattedByMonth: t.datetime.format('YYYY [>] MMMM'),
+    formattedByDay: t.datetime.format('dddd Do'),
+  }));
+
+  const val = groupingStyle === GROUPING_STYLE.BY_DATE ?
+    'formattedByDay' : 'formattedByMonth';
+
+  const sorted = _.sortBy(dateMapped, val).reverse();
+  const grouped = _.groupBy(sorted, val);
+
+  const returnable = [];
+  for (const key in grouped) {
+    returnable.push(grouped[key]);
+  }
+
+  return returnable;
+}
+
 export const TransactionViewComponent: React.SFC<TransactionViewProps> = (props) => {
-
-  function groupTransactionsByMonth() {
-    const dateMapped = props.transactions.map(t => ({
-      ...t,
-      formattedByMonth: t.datetime.format('YYYY [>] MMMM'),
-    }));
-
-    const sorted = _.sortBy(dateMapped, 'formattedByMonth').reverse();
-    const groupingByMonth = _.groupBy(sorted, 'formattedByMonth');
-
-    const returnable = [];
-    for (const key in groupingByMonth) {
-      returnable.push(groupingByMonth[key]);
-    }
-
-    return returnable;
-  }
-
-  function groupTransactionsByDay(transactions: Transaction[]) {
-    const dateMapped = transactions.map(t => ({
-      ...t,
-      formattedByDay: t.datetime.format('dddd Do'),
-    }));
-
-    const sorted = _.sortBy(dateMapped, 'formattedByDay').reverse();
-    const groupingByDay = _.groupBy(sorted, 'formattedByDay');
-
-    const returnable = [];
-    for (const key in groupingByDay) {
-      returnable.push(groupingByDay[key]);
-    }
-
-    return returnable;
-  }
 
   function addNewTransaction() {
 
@@ -121,13 +114,13 @@ export const TransactionViewComponent: React.SFC<TransactionViewProps> = (props)
     <React.Fragment>
       <button onClick={addNewTransaction}>New transaction</button>
       <Container>
-        {groupTransactionsByMonth().map((trs) => {
+        {groupTransactions(props.transactions, GROUPING_STYLE.BY_MONTH).map((trs) => {
           const monthTitle = trs[0].formattedByMonth;
           return (
             <React.Fragment key={monthTitle}>
               <MonthTitle>{monthTitle}</MonthTitle>
               <React.Fragment>
-                {groupTransactionsByDay(trs).map((trs2) => {
+                {groupTransactions(trs, GROUPING_STYLE.BY_DATE).map((trs2) => {
                   const dayTitle = trs2[0].formattedByDay;
                   return (
                     <React.Fragment key={dayTitle}>
